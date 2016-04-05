d1f12088222473018b9c2d0db3d84ea5ce036ddd
jaideng123 <jaideng123@yahoo.com>
2016-04-05 11:06:41 -0500

    Trying to get this deployment up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 
-http.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {
 
    // Send the HTTP header 
    // HTTP Status: 200 : OK
@@ -8,8 +8,10 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+   response.end('<h1>Hello World</h1>');
+});
+var port = Number(process.env.PORT || 3000)
+
+server.listen(port);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
