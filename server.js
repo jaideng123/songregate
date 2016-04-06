e0673b0d301a194419d1ae30e67dec6257a7c7cc
jaideng123 <jaideng123@yahoo.com>
2016-04-06 10:09:36 -0500

    Now throws randm song at you

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var http = require("http");
+var fs = require('fs');
 
+var counter = 0
+var songs = []
+fs.readFile('Songs.json', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  songs = JSON.parse(data);
+  console.log("finished parsing")
+});
 var server = http.createServer(function (request, response) {
 
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
+   var rand = Math.floor((Math.random() * Number(songs.length))); 
    // Send the response body as "Hello World"
-   response.end('<h1>Hello World</h1>');
+   response.end(JSON.stringify({'name':songs[rand].name,'artist':songs[rand]['artists'][0].name}));
 });
 var port = Number(process.env.PORT || 3000)
 
