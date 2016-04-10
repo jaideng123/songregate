5ec994fd0302af9fbafd97b6f41fcdaa35869e39
AndrewSchlotzhauer <juggernot1@tamu.edu>
2016-04-09 21:08:52 -0500

    added a few users

--- seed.js
+++ seed.js
@@ -4,7 +4,7 @@ exports.preMadeUsers = function(songs){
   //franklin likes hip hop
   users.push(exports.createSeedUser(['Kanye West','Jay Z','DMX','J. Cole','Akon','A$AP Rocky','Kendrick Lamar','Drake','Frank Ocean'],['David Bowie','A$AP Ferg','Justin Bieber'],songs,false));
   //brenda likes popular female artists
-  users.push(exports.createSeedUser(['Ke$ha','Katy Perry','Lana Del Rey','Avril Lavigne','Ariana Grande','Carly Rae Jepsen','Icona Pop','Beyoncé','Adele'],['DMX'],songs,false));
+  users.push(exports.createSeedUser(['Ke$ha','Katy Perry','Lana Del Rey','Avril Lavigne','Ariana Grande','Carly Rae Jepsen','Icona Pop','Beyoncé','Adele','Selena Gomez'],['DMX'],songs,false));
   //nathan likes indie artists
   users.push(exports.createSeedUser(['Bon Iver','AWOLNATION','BØRNS','Cage The Elephant','Fleetwood Mac','Grouplove','Icona Pop','John Mayer','Kendrick Lamar','alt-J'],['Ke$ha','Ariana Grande','Justin Bieber'],songs,false));
   //keith likes country
@@ -15,6 +15,10 @@ exports.preMadeUsers = function(songs){
   users.push(exports.createSeedUser(['Bruce Springsteen','David Bowie','AC/DC','Aerosmith','Arctic Monkeys','Def Leppard','Guns N\' Roses','Imagine Dragons','Led Zeppelin','Michael Jackson','The Beatles','The Smashing Pumpkins','Jimi Hendrix'],['DJ Luke Nasty','Ke$ha'],songs,false));
   //samantha is in middle school
   users.push(exports.createSeedUser(['John Mayer','Bring Me The Horizon','Bright Eyes','Bon Iver','Capital Cities','Coldplay','Demi Lovato','Ellie Goulding','Fall Out Boy','Evanescence','Foster The People','Green Day','blink-182','WALK THE MOON'],['Guns N\' Roses'],songs,false));
+  //clay likes more modern artists and leans to more pop songs
+  users.push(exports.createSeedUser(['Carly Rae Jepsen','Carrie Underwood','Coldplay','Adele','Avicii','Demi Lovato','Ed Sheeran','Icona Pop','Katy Perry','Maroon 5','OneRepublic'],['Daft Punk', 'Def Leppard','AC/DC','Jimi Hendrix'],songs,false));
+  //chris likes rock artiists
+  users.push(exports.createSeedUser(['Def Leppard','AC/DC','Jimi Hendrix','Phil Collins','Led Zeppelin','Journey','The Strokes','Train','Aerosmith','Bob Dylan','Guns N\' Roses'],['Rachel Platten','Selena Gomez','T-Pain'],songs,false));
   return users;
 }
 var psuedocount = 0;
