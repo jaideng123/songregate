6135115d9b7b4c57774aca96ae7e13fc53d98a01
AndrewSchlotzhauer <juggernot1@tamu.edu>
2016-04-18 21:21:42 -0500

    First attempt-Mainly generate random userID

--- js/controllers.js
+++ js/controllers.js
@@ -16,5 +16,22 @@ songregateControllers.controller('MusicCtrl', ['$scope', '$routeParams',
 
 songregateControllers.controller('StartCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
-
+    $scope.userID = makeid()
+	  
+	  function makeid()
+	  {
+		  var text = "";
+		  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		  
+		  for( var i=0; i < 5; i++ )
+			  text += possible.charAt(Math.floor(Math.random() * possible.length));
+		  
+		  //check if that id has been generated already (maybe needed?)
+		  /* this part requires server and backend to be working
+		  if (text == Other User's ID)
+			  text = makeid()
+		  */
+		  return text;
+		}
+		
   }]);
