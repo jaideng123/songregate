9d1e1348f8618d4fee569622fe977d8f640b3a22
AndrewSchlotzhauer <juggernot1@tamu.edu>
2016-04-20 01:06:22 -0500

    Added some controller stuff
    Just some simple additions so it is a little less hard-coded and easier to change later  on when the server works.

--- js/controllers.js
+++ js/controllers.js
@@ -6,34 +6,41 @@ var songregateControllers = angular.module('songregateControllers', []);
 
 songregateControllers.controller('WelcomeCtrl', ['$scope',
   function($scope) {
-    
+	
   }]);
 
 songregateControllers.controller('MusicCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
-    
+    //just hardcoded for now until server is up
+    $scope.currentsong = "22"
+	$scope.artist = "Taylor Swift"
+	$scope.album = "1989"
+	$scope.artURL = "http://s.newsweek.com/sites/www.newsweek.com/files/2014/10/26/image-1989.png"
+	
   }]);
 
 songregateControllers.controller('StartCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
-    $scope.artists = []
+	//for GUI check purposes artists are hardcoded but will be random from server-side
+    $scope.artists = ['Taylor Swift','Kanye West','Keith Urban','Avicii','AWOL Nation']
+	
     $scope.userID = makeid()
 	  
     //this whole function will be server-side once we get it up and running
-	  function makeid()
-	  {
-		  var text = "";
-		  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-		  
-		  for( var i=0; i < 5; i++ )
-			  text += possible.charAt(Math.floor(Math.random() * possible.length));
-		  
-		  //check if that id has been generated already (maybe needed?)
-		  /* this part requires server and backend to be working
-		  if (text == Other User's ID)
-			  text = makeid()
-		  */
-		  return text;
-		}
+	function makeid()
+	{
+	   var text = "";
+	   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	  
+	   for( var i=0; i < 5; i++ )
+		   text += possible.charAt(Math.floor(Math.random() * possible.length));
+	  
+	   //check if that id has been generated already (maybe needed?)
+	   /* this part requires server and backend to be working
+	   if (text == Other User's ID)
+	   	   text = makeid()
+	   */
+	   return text;
+	}
 		
   }]);
