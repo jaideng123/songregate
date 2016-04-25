cf67a205ae50330aa8354363fa6e8dd067160c8f
Jaiden Gerig <jaidengerig@gmail.com>
2016-04-25 11:00:04 -0500

    Started hooking things together

--- js/controllers.js
+++ js/controllers.js
@@ -10,22 +10,59 @@ songregateControllers.controller('WelcomeCtrl', ['$scope',
 	
   }]);
 
-songregateControllers.controller('MusicCtrl', ['$scope', '$routeParams', '$location',
-  function($scope, $routeParams, $location) {
-    //just hardcoded for now until server is up
-    $scope.currentsong = "22"
-	$scope.artist = "Taylor Swift"
-	$scope.album = "1989"
-	$scope.artURL = "http://s.newsweek.com/sites/www.newsweek.com/files/2014/10/26/image-1989.png"
-	$scope.userid = $location.path().split('/')[2] //will be in form ['','music','userid']
+songregateControllers.controller('MusicCtrl', ['$scope', '$routeParams', '$location','$http',
+  function($scope, $routeParams, $location,$http) {
+    $scope.userid = $location.path().split('/')[2] //will be in form ['','music','userid']
+    $http({
+		  method: 'GET',
+		  url: api_url+'song/current?url=true'
+		}).then(function successCallback(response) {
+		    $scope.current_song = response.data
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+	$scope.nextSong = function(){
+		$http({
+		  method: 'GET',
+		  url: api_url+'song/next?url=true'
+		}).then(function successCallback(response) {
+		    $scope.current_song = response.data
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+	}
+	$scope.pausePlay = function(){
+		
+	}
+	$scope.like = function(){
+		$http({
+		  method: 'GET',
+		  url: api_url+'users/'+$scope.userId+'/likes?song='+current_song.playId
+		}).then(function successCallback(response) {
+		    $scope.current_song = response.data
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+	}
+	$scope.dislike = function(){
+		$http({
+		  method: 'GET',
+		  url: api_url+'users/'+$scope.userId+'/dislikes?song='+current_song.playId
+		}).then(function successCallback(response) {
+		    $scope.current_song = response.data
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+	}
   }]);
-
 songregateControllers.controller('StartCtrl', ['$scope', '$routeParams','$http',
   function($scope, $routeParams, $http) {
 	//for GUI check purposes artists are hardcoded but will be random from server-side
     $scope.artists = ['Taylor Swift','Kanye West','Keith Urban','Avicii','AWOL Nation']
-	
-    $scope.user = {}
     $http({
 		  method: 'GET',
 		  url: api_url+'users/new'
